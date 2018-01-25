2c19afb6ffafc71b60bae1b60ed96472b8bac56e
Martin Schrimpf <m4rtinsch@gmail.com>
2018-01-25 15:36:26 -0500

    generically extract directories and move to features dir with mapping

--- feature_extractors/alexnet/finetune_alexnet_with_tensorflow/merge_features.py
+++ feature_extractors/alexnet/finetune_alexnet_with_tensorflow/merge_features.py
@@ -37,9 +37,17 @@ def convert_matrix(image_features):
     return np.array(features)
 
 
+def extract(image_features, image_directory):
+    filter_fun = lambda img_features: os.path.basename(os.path.split(img_features[0])[0]) == image_directory
+    result = list(filter(filter_fun, image_features))
+    if len(result) == 0:
+        raise ValueError('no image features found for directory {}'.format(image_directory))
+    return result
+
+
 def save(features, features_directory):
-    target_filepath = os.path.join(os.path.dirname(__file__), '..', '..', '..', '..', 'data', 'features',
-                                   features_directory, 'alexnet-finetune-relu7.mat')
+    features_root_directory = os.path.join(os.path.dirname(__file__), '..', '..', '..', '..', 'data', 'features')
+    target_filepath = os.path.join(features_root_directory, features_directory, 'alexnet-finetune-relu7.mat')
     scipy.io.savemat(target_filepath, {'features': features})
     return target_filepath
 
@@ -51,31 +59,31 @@ def process(image_features, save_directory):
     logger.info('Saved {} to {}'.format(features.shape, savepath))
 
 
-def extract(image_features, image_directory):
-    filter_fun = lambda img_features: os.path.basename(os.path.split(img_features[0])[0]) == image_directory
-    result = list(filter(filter_fun, image_features))
-    if len(result) == 0:
-        raise ValueError('no image features found for directory {}'.format(image_directory))
-    return result
-
-
 def main():
+    type_dir_mapping = {'occluded': 'data_occlusion_klab325v2',
+                        'whole': 'klab325_orig',
+                        'lessOcclusion': 'lessOcclusion'}
+
     parser = argparse.ArgumentParser()
     parser.add_argument('--log_level', type=str, default='INFO')
-    parser.add_argument('--directory', type=str, default='images')
+    parser.add_argument('--file_pattern', type=str, default='images/predictions*.txt')
     args = parser.parse_args()
     logging.basicConfig(stream=sys.stdout, level=logging.getLevelName(args.log_level))
     logger.info("Running with args %s", vars(args))
 
-    features_files = glob.glob(os.path.join(args.directory, 'predictions*.txt'))
+    features_files = glob.glob(args.file_pattern)
     logger.info('Loading from {} files'.format(len(features_files)))
     image_features = [load(features_file) for features_file in features_files]
     image_features = merge_lists(image_features)
     logger.info('Loaded features of size {}'.format(len(image_features)))
-    occluded_features = extract(image_features, 'occluded')
-    whole_features = extract(image_features, 'whole')
-    process(occluded_features, 'data_occlusion_klab325v2')
-    process(whole_features, 'klab325_orig')
+
+    def get_last_directory(path):
+        return os.path.split(os.path.split(path)[0])[-1]
+
+    for type in np.unique([get_last_directory(imagepath) for imagepath, _ in image_features]):
+        assert type in type_dir_mapping
+        features = extract(image_features, type)
+        process(features, type_dir_mapping[type])
 
 
 if __name__ == '__main__':
